3aeb870285c588b28e33ec72a3dbe644cda4ba0b
madhava <madhavam2209@gmail.com>
2023-12-10 12:38:08 +0530

    restaurant menus added

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -12,12 +12,28 @@ export const Menu = () => {
     const fetchMenu = async () =>{
         const data = await fetch( MENU_URL + resId)
         const jsonData = await data.json() 
-        console.log('json', jsonData.data.cards[0].card.card)
+        console.log('json', jsonData.data.cards)
         setResInfo(jsonData.data.cards)
     }
-    return (resInfo.length === 0) ? <ShimmerUI/> : (
+    if(resInfo.length === 0) return <ShimmerUI/>
+    const {name, costForTwoMessage, cuisines} = resInfo[0].card.card.info
+    const resMenu = resInfo[2].groupedCard.cardGroupMap.REGULAR.cards[1].card.card.itemCards
+    return (
         <div>
-            <h1>{resInfo[0].card.card.info.name}</h1>
+            <h1>{name}</h1>
+            <h5>{cuisines.join(', ')}</h5>
+            <p>{costForTwoMessage}</p>
+            <br /><br />
+            {
+                resMenu !== undefined ? 
+            <ul>
+                {resMenu.map(res =>{
+                    return (
+                        <li key={res.card.info.id}><b>{res.card.info.name}</b> - <i><b>Rs:</b> {res.card.info.price/100}</i></li>
+                    )
+                })}
+            </ul> : null
+            }
         </div>
     )
 }
